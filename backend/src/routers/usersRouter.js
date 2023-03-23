5cc1fc61f148b8b29e0ce6737ca23f6361d949e7
greenanand <116130877+anand1350@users.noreply.github.com>
2023-03-23 17:39:12 +0800

    transition

---
+++ backend/src/routers/usersRouter.js
@@ -0,0 +1,39 @@
+import express from "express";
+import verifyToken from "../middlewares/verifyToken";
+import {
+  createUser,
+  deleteUser,
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../services/usersService";
+
+const usersRouter = express.Router();
+
+usersRouter.get("/", verifyToken, async (req, res) => {
+  console.log("user is :", req.user);
+  res.json(await getUsers());
+});
+
+usersRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  res.json(await getUserById(id));
+});
+
+usersRouter.post("/", async (req, res) => {
+  const user = req.body;
+  res.json(await createUser(user));
+});
+
+usersRouter.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = req.body;
+  res.json(await updateUser(id, user));
+});
+
+usersRouter.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  res.json(await deleteUser(id));
+});
+
+export default usersRouter;
